2fb389ae876049ee6bc5eae289eface57d87afaa
PRAJYOT-PC\PRAJYOT <prajyot@bittint.com>
2018-12-09 20:18:47 +0530

    table component completed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import {Pagination,Table} from 'ant-design-vue';
+import Antd from 'ant-design-vue';
+Vue.use(Table)
+Vue.use(Pagination)
+Vue.use(Antd)
 Vue.config.productionTip = false
-
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'ant-design-vue/dist/antd.css';
 new Vue({
   render: h => h(App),
 }).$mount('#app')
